b20d3f4b49a92d951efdb1e8370a45b8511ee55e
Owen <owenventer99@gmail.com>
2023-02-02 22:19:55 +0200

    adding company task buttons

--- src/pages/company/[companyName].tsx
+++ src/pages/company/[companyName].tsx
@@ -26,15 +26,21 @@ export function EmployerDashView({ }){
   return (
 
     <div className="md:hero mx-auto p-2 m-5">
+      {/* navigation buttons in top corners  */}
       <button className=" absolute top-20 right-10 shadow-lg bg-black text-neutral-content bg-opacity-50 rounded text-gray-100 py-2 px-2 "> My Employee Dashboard</button>
       <Link href="/"> <button className=" absolute top-20 left-10 shadow-lg bg-black text-neutral-content bg-opacity-50 rounded text-gray-100 py-2 px-2 ">Back</button> </Link>
+      {/* Main div */}
       <div className="md:hero-content flex flex-col  items-center shadow-lg bg-black text-neutral-content bg-opacity-50 rounded mt-3">
         <Image src={imgLink} alt='chronoLabsMainLogo' width={250} height={250}/>
-          
-        
-      
-        
-       
+        {/* Button div */}
+        <div className="grid-cols-5">
+        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
+        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
+        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
+        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
+        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
+
+        </div>
       </div>
     </div>
   );
