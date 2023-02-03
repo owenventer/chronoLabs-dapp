17a653323c477909641d26c1ad3e1f186c607539
Owen <owenventer99@gmail.com>
2023-02-03 14:02:29 +0200

    fixing UI styling

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -7,7 +7,7 @@ export default function CompanyCard({companyName}){
         <div className="flex flex-col items-center ">
             <Image src={imgLink} alt='chronoLabsMainLogo' width={250} height={250}/>
             <Link href={`company/${companyName}`}>
-                <button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 rounded"> Manage</button>
+                <button className="bg-[#14F195] hover:hover:scale-105 text-black font-bold py-2 px-2 rounded"> Manage</button>
             </Link>
             
           </div>
--- src/pages/company/[companyName].tsx
+++ src/pages/company/[companyName].tsx
@@ -25,7 +25,7 @@ export function EmployerDashView({ }){
   const imgLink="/"+companyName+"Logo.png"
   return (
 
-    <div className="md:hero mx-auto p-2 m-5">
+    <div className="md:hero mx-auto p-2 m-10">
       {/* navigation buttons in top corners  */}
       <button className=" absolute top-20 right-10 shadow-lg bg-black text-neutral-content bg-opacity-50 rounded text-gray-100 py-2 px-2 "> My Employee Dashboard</button>
       <Link href="/"> <button className=" absolute top-20 left-10 shadow-lg bg-black text-neutral-content bg-opacity-50 rounded text-gray-100 py-2 px-2 ">Back</button> </Link>
@@ -33,12 +33,12 @@ export function EmployerDashView({ }){
       <div className="md:hero-content flex flex-col  items-center shadow-lg bg-black text-neutral-content bg-opacity-50 rounded mt-3">
         <Image src={imgLink} alt='chronoLabsMainLogo' width={250} height={250}/>
         {/* Button div */}
-        <div className="grid-cols-5">
-        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
-        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
-        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
-        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
-        <div><button className="bg-[#14F195] hover:bg-[#9abaad] text-black font-bold py-2 px-2 m-3 rounded"> Daily Reports</button></div>
+        <div className="grid grid-cols-2 m-1">
+        <div><button className="bg-[#14F195] hover:scale-105 text-black font-bold p-2 w-2/3 m-2 mx-10 rounded"> Daily Reports</button></div>
+        <div><button className="bg-[#14F195] hover:scale-105 text-black font-bold p-2 w-2/3 m-2 mx-10 rounded"> Employee Reports</button></div>
+        <div><button className="bg-[#14F195] hover:scale-105 text-black font-bold p-2 w-2/3 m-2 mx-10 rounded"> Tax Reports</button></div>
+        <div><button className="bg-[#14F195] hover:scale-105 text-black font-bold p-2 w-2/3 m-2 mx-10 rounded"> Daily Activity</button></div>
+        <div><button className="bg-[#14F195] hover:scale-105 text-black font-bold p-2 w-2/3 m-2 mx-10 rounded"> Manage Employee</button></div>
 
         </div>
       </div>
